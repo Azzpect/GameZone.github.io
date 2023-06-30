27ad5287a145e84006622026ecad2a49661a923e
AJ6574 <atanughosh.brp@gmail.com>
2023-07-01 01:36:46 +0530

    Added game over window

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const box = document.querySelectorAll('.box')
         let turn = 0
+        let game_over = false
+        let msg = ''
         box.forEach(element => {
             element.addEventListener('click', () => {
                 if(element.innerHTML == ''){
@@ -13,43 +15,68 @@ const box = document.querySelectorAll('.box')
                         box[0].style.textDecoration = 'line-through'
                         box[1].style.textDecoration = 'line-through'
                         box[2].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[0].innerHTML}' is the winner`
                     }
                     else if((box[3].innerHTML == box[4].innerHTML) && (box[3].innerHTML == box[5].innerHTML) && (box[3].innerHTML != '')){
                         box[3].style.textDecoration = 'line-through'
                         box[4].style.textDecoration = 'line-through'
                         box[5].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[3].innerHTML}' is the winner`
                     }
                     else if((box[6].innerHTML == box[7].innerHTML) && (box[6].innerHTML == box[8].innerHTML) && (box[6].innerHTML != '')){
                         box[6].style.textDecoration = 'line-through'
                         box[7].style.textDecoration = 'line-through'
                         box[8].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[6].innerHTML}' is the winner`
                     }
                     else if((box[0].innerHTML == box[3].innerHTML) && (box[0].innerHTML == box[6].innerHTML) && (box[0].innerHTML != '')){
                         box[0].style.textDecoration = 'line-through'
                         box[3].style.textDecoration = 'line-through'
                         box[6].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[0].innerHTML}' is the winner`
                     }
                     else if((box[1].innerHTML == box[4].innerHTML) && (box[1].innerHTML == box[7].innerHTML) && (box[1].innerHTML != '')){
                         box[1].style.textDecoration = 'line-through'
                         box[4].style.textDecoration = 'line-through'
                         box[7].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[1].innerHTML}' is the winner`
                     }
                     else if((box[2].innerHTML == box[5].innerHTML) && (box[2].innerHTML == box[8].innerHTML) && (box[2].innerHTML != '')){
                         box[2].style.textDecoration = 'line-through'
                         box[5].style.textDecoration = 'line-through'
                         box[8].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[2].innerHTML}' is the winner`
                     }
                     else if((box[0].innerHTML == box[4].innerHTML) && (box[0].innerHTML == box[8].innerHTML) && (box[0].innerHTML != '')){
                         box[0].style.textDecoration = 'line-through'
                         box[4].style.textDecoration = 'line-through'
                         box[8].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[0].innerHTML}' is the winner`
                     }
                     else if((box[2].innerHTML == box[4].innerHTML) && (box[2].innerHTML == box[6].innerHTML) && (box[2].innerHTML != '')){
                         box[2].style.textDecoration = 'line-through'
                         box[4].style.textDecoration = 'line-through'
                         box[6].style.textDecoration = 'line-through'
+                        game_over = true
+                        msg = `'${box[2].innerHTML}' is the winner`
+                    }
+                    if(game_over)
+                    {
+                        document.getElementsByClassName('game-over')[0].style.display = 'flex'
+                        document.querySelector('.game-over>p').innerHTML = msg
                     }
                     turn++
                 }
             })
         });
+
+function restart(){
+    window.location.reload()
+}
